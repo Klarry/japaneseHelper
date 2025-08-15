c3646329fc2337f8a0df2e55f83091a59567d5c3
Klara Zyrianova <lara.zyryanova@gmail.com>
2025-08-15 14:00:05 +0400

    Updated compileSdk and libraries.

--- app/src/main/java/com/japanesehelper/presentation/viewmodel/VocabViewModel.kt
+++ app/src/main/java/com/japanesehelper/presentation/viewmodel/VocabViewModel.kt
@@ -25,6 +25,10 @@ class VocabViewModel @Inject constructor(
     private val searchRepository: GoogleSearchRepository
 ) : ViewModel() {
 
+    object HttpStatus {
+        const val TOO_MANY_REQUESTS = 429
+    }
+
     private val _randomWord = MutableStateFlow<RandomWord?>(null)
     val randomWord = _randomWord.asStateFlow()
 
@@ -35,17 +39,27 @@ class VocabViewModel @Inject constructor(
         getRandomWord()
     }
 
-    fun getRandomWord(
+    fun getRandomWord() {
+        viewModelScope.launch {
+            try {
+                _randomWord.value = vocabRepository.getRandomWord()
+            } catch (e: Exception) {
+                Log.d("getRandomWord", "Exception thrown: ${ e.message }, cause: ${ e.cause }, localizedMessage: ${ e.localizedMessage }")
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun getPictureData(
         apiKey: String = "",
         cx: String = "",
     ) {
+        if (apiKey.isEmpty() || cx.isEmpty()) return
+
         _pictureData.value = PictureLoading()
 
         viewModelScope.launch {
-
             try {
-                _randomWord.value = vocabRepository.getRandomWord()
-
                 val searchResult = getSearchResultAsync(
                     apiKey = apiKey,
                     cx = cx,
@@ -63,14 +77,10 @@ class VocabViewModel @Inject constructor(
                     }
                     else -> _pictureData.value = PictureError(e.message.orEmpty())
                 }
-                Log.d("getRandomWord", "Exception thrown: ${ e.message }, cause: ${ e.cause }, localizedMessage: ${ e.localizedMessage }")
+                Log.d("getPictureData", "Exception thrown: ${ e.message }, cause: ${ e.cause }, localizedMessage: ${ e.localizedMessage }")
                 e.printStackTrace()
             }
         }
-    }
-
-    object HttpStatus {
-        const val TOO_MANY_REQUESTS = 429
     }
 
     suspend fun getSearchResultAsync(
